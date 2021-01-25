d12672d0ae54fcf1600b091e653bc9c1c532cc76
catootje <catodekruif@gmail.com>
2021-01-25 20:35:56 +0100

    input from users is seen on the site!!!!!

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('', home_view, name='home'),
     path('eet/<date>/<name>', eetMee, name='eet'),
-    path('show/<date>/<name>', showEetmee, name='show')
 ]
