93260b051d642374f1e8cf8893fb1de52113708b
catootje <catodekruif@gmail.com>
2021-01-27 20:18:20 +0100

    removed sign up option

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,10 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
 from django.views.generic.base import RedirectView
 from django.conf import settings
-from .views import SignUpView, home_view, eetMee, inschrijven
+from .views import home_view, eetMee, inschrijven
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    #path('signup/', SignUpView.as_view(), name='signup'),
     path('', home_view, name='home'),
     path('eet/<date>/<name>', eetMee, name='eet'),
     path('inschrijven/<date>/<name>', inschrijven, name='inschrijven')
--- accounts/views.py
+++ accounts/views.py
@@ -7,10 +7,10 @@ from .models import Housemate, Cook, Housemate_eats
 from .forms import EetMeeForm, inschrijvenForm
 
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+#class SignUpView(generic.CreateView):
+#    form_class = UserCreationForm
+#    success_url = reverse_lazy('login')
+#    template_name = 'registration/signup.html'
 
 
 def home_view(request):
